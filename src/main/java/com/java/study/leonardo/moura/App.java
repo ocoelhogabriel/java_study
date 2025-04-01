15b7bb2bfcf9ff1b74a6e38eb459d97a4cb2d40b
gabriel.coelho <gabriel.coelho@telematica.com.br>
2025-04-01 09:21:54 -0300

    Init

---
+++ src/main/java/com/java/study/leonardo/moura/App.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.java.study.leonardo.moura;
+
+/**
+ * Curso: Java COMPLETO: Do Zero ao Profissional + Projetos!
+ * Professor: Leonardo Moura - Cod3r Cursos Online
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
